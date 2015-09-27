4ab19341ebd966e0d361d631963127b2e68d0859
MxMcG <mmcgeevibes@gmail.com>
2015-09-26 17:23:55 -0700

    add animated scroll

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+
+        var target = $(this.hash);
+
+
+        $('html, body').stop().animate({
+            'scrollTop': target.offset().top
+        }, 900, 'swing');
+    });
+
+
 });
