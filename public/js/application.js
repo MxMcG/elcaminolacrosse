499993165fcd1c0f102cdeb4d097e569870d7677
dimarinez <dimarinez@yahoo.com>
2015-09-28 22:58:23 -0700

    roster

--- public/js/application.js
+++ public/js/application.js
@@ -13,5 +13,25 @@ $(document).ready(function() {
         }, 900, 'swing');
     });
 
+  $(window).scroll( function(){
+
+
+
+    $('.fadein').each( function(i) {
+
+    	var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+   if( bottom_of_window > bottom_of_object ){
+                
+        $(this).animate({'opacity':'1'},700);
+                    
+      	}
+    });
+
+  });
+
+
+
 
 });
